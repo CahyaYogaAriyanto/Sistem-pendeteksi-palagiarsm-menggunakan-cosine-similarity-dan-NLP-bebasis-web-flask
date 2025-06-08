fb9ce30847e09b564966b04898013b4df7652785
denyocr <denyocr.world@gmail.com>
2025-06-08 23:45:45 +0700

    first commit

--- myapp.py
+++
@@ -1,7 +0,0 @@
-import pdfkit
-
-config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
-
-html = "<h1>Hello, PDF!</h1><p>Ini adalah percobaan.</p>"
-
-pdfkit.from_string(html, "hasil_test.pdf", configuration=config)
